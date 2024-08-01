b9ea8aa47556506eee603f92fc8246888491c33b
Andrew Osei <andrewosei94@gmail.com>
2024-08-01 14:57:20 +0000

    minor Ui fix on project card component

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -7,15 +7,14 @@ import React from "react";
 
 const ProjectCard = ({ excerpt, href, image, name, slug }: IProject) => {
   return (
-    <div className="relative h-[411px] aspect-square">
+    <div className="relative">
       <Image
-        alt="Project Image"
         src={image}
-        placeholder="blur"
-        quality={100}
-        fill
+        alt={"Proffer Aid International Foundation"}
+        width={411}
+        height={411}
+        className="object-cover rounded-3xl aspect-square"
         sizes="(max-width: 1200px) 100vw, (max-width: 768px) 50vw, 33vw"
-        className="object-cover rounded-3xl bg-center"
       />
 
       <div className="absolute left-0 right-0 top-0 bottom-0 bg-dark/60 rounded-3xl p-12 text-white flex flex-col flex-1 gap-4">
